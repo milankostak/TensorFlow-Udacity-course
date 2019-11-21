54eb8752eb51a50c49f734aa725502fc0c4f86b3
Milan Košťák <milankostak@seznam.cz>
2019-11-21 16:31:42 +0100

    Lesson 2 - c2f.py - print layer values, small changes

--- Lesson 2 - Celsius to Fahrenheit/c2f.py
+++ Lesson 2 - Celsius to Fahrenheit/c2f.py
@@ -4,22 +4,22 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-tf.logging.set_verbosity(tf.logging.ERROR)
+# tf.logging.set_verbosity(tf.logging.ERROR)
 
 celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
 # for i, c in enumerate(celsius_q):
-#     print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
+#     print("{} °C = {} °F".format(c, fahrenheit_a[i]))
 
-# l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
-# model = tf.keras.Sequential([l0])
+l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
+model = tf.keras.Sequential([l0])
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(units=1, input_shape=[1])
-    # tf.keras.layers.Dense(units=2, input_shape=[1]),
-    # tf.keras.layers.Dense(units=1, input_shape=[2])
-])
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(units=1, input_shape=[1])
+#     tf.keras.layers.Dense(units=2, input_shape=[1]),
+#     tf.keras.layers.Dense(units=1, input_shape=[2])
+# ])
 
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
 
@@ -32,3 +32,5 @@ plt.plot(history.history['loss'])
 plt.show()
 
 print(model.predict(np.array([22])))  # 71.6
+
+print("These are the l0 variables: {}".format(l0.get_weights()))
