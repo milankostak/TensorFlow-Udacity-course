4a2568c3d55ffc58df5f328b1f9cecba92dc9408
Milan Košťák <milankostak@seznam.cz>
2019-11-22 10:04:35 +0100

    Lesson 6 - Transfer Learning

---
+++ Lesson 6 - Transfer Learning/flowers_2.py
@@ -0,0 +1,177 @@
+# exercise
+# https://colab.research.google.com/github/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_for_deep_learning/l06c02_exercise_flowers_with_transfer_learning.ipynb
+# solution
+# https://colab.research.google.com/github/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_for_deep_learning/l06c03_exercise_flowers_with_transfer_learning_solution.ipynb
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+from tensorflow.keras import layers
+
+import numpy as np
+import matplotlib.pyplot as plt
+import logging
+
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+#
+# Download the Flowers Dataset using TensorFlow Datasets
+#
+
+splits = tfds.Split.TRAIN.subsplit([70, 30])
+
+(training_set, validation_set), dataset_info = tfds.load('tf_flowers', with_info=True, as_supervised=True, split=splits)
+
+#
+# Print Information about the Flowers Dataset
+#
+
+num_classes = dataset_info.features['label'].num_classes
+
+num_training_examples = 0
+num_validation_examples = 0
+
+for example in training_set:
+    num_training_examples += 1
+
+for example in validation_set:
+    num_validation_examples += 1
+
+print('Total Number of Classes: {}'.format(num_classes))
+print('Total Number of Training Images: {}'.format(num_training_examples))
+print('Total Number of Validation Images: {} \n'.format(num_validation_examples))
+
+for i, example in enumerate(training_set.take(5)):
+    print('Image {} shape: {} label: {}'.format(i + 1, example[0].shape, example[1]))
+
+#
+# Reformat Images and Create Batches
+#
+
+IMAGE_RES = 224
+
+
+def format_image(image, label):
+    image = tf.image.resize(image, (IMAGE_RES, IMAGE_RES)) / 255.0
+    return image, label
+
+
+BATCH_SIZE = 32
+
+train_batches = training_set.shuffle(num_training_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+validation_batches = validation_set.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+#
+# Do Simple Transfer Learning with TensorFlow Hub
+#
+
+URL = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
+feature_extractor = hub.KerasLayer(URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
+
+feature_extractor.trainable = False
+
+model = tf.keras.Sequential([
+    feature_extractor,
+    layers.Dense(num_classes, activation='softmax')
+])
+
+model.summary()
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+EPOCHS = 6
+
+history = model.fit(train_batches, epochs=EPOCHS, validation_data=validation_batches)
+
+#
+# Plot Training and Validation Graphs
+#
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(EPOCHS)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+#
+# Check Predictions
+#
+
+class_names = np.array(dataset_info.features['label'].names)
+print("Class names: ", class_names)
+
+image_batch, label_batch = next(iter(train_batches))
+image_batch = image_batch.numpy()
+label_batch = label_batch.numpy()
+
+predicted_batch = model.predict(image_batch)
+predicted_batch = tf.squeeze(predicted_batch).numpy()
+predicted_ids = np.argmax(predicted_batch, axis=-1)
+predicted_class_names = class_names[predicted_ids]
+print("Predicted class names: ", predicted_class_names)
+
+print("Labels:           ", label_batch)
+print("Predicted labels: ", predicted_ids)
+
+plt.figure(figsize=(10, 9))
+for n in range(30):
+    plt.subplot(6, 5, n + 1)
+    plt.subplots_adjust(hspace=0.3)
+    plt.imshow(image_batch[n])
+    color = "blue" if predicted_ids[n] == label_batch[n] else "red"
+    plt.title(predicted_class_names[n].title(), color=color)
+    plt.axis('off')
+_ = plt.suptitle("Model predictions (blue: correct, red: incorrect)")
+plt.show()
+
+#
+# Perform Transfer Learning with the Inception Model
+#
+
+IMAGE_RES = 299
+
+(training_set, validation_set), dataset_info = tfds.load('tf_flowers', with_info=True, as_supervised=True, split=splits)
+train_batches = training_set.shuffle(num_training_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+validation_batches = validation_set.map(format_image).batch(BATCH_SIZE).prefetch(1)
+
+URL = "https://tfhub.dev/google/tf2-preview/inception_v3/feature_vector/4"
+feature_extractor = hub.KerasLayer(URL, input_shape=(IMAGE_RES, IMAGE_RES, 3), trainable=False)
+
+model_inception = tf.keras.Sequential([
+    feature_extractor,
+    tf.keras.layers.Dense(num_classes, activation='softmax')
+])
+
+model_inception.summary()
+
+model_inception.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+EPOCHS = 6
+
+history = model_inception.fit(train_batches, epochs=EPOCHS, validation_data=validation_batches)
