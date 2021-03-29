16d0db7cb6ef7a7cbe7b497a5294b212814e26d6
Milan Košťák <milankostak@seznam.cz>
2021-03-29 15:25:46 +0200

    c2f.py - better formatting and other improvements

--- Lesson 2 - Celsius to Fahrenheit/c2f.py
+++ Lesson 2 - Celsius to Fahrenheit/c2f.py
@@ -4,14 +4,21 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-# tf.logging.set_verbosity(tf.logging.ERROR)
+# generate train data
+celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 
-celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
-fahrenheit_a = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+# the right output for the train data
+# (usually you will not calculate those in runtime, but they will be provided in the dataset)
+fahrenheit = [t * 1.8 + 32 for t in celsius]
+fahrenheit = np.array(fahrenheit, dtype=float)
+print(fahrenheit)
+# -40, 14, 32, 46, 59, 72, 100
 
-# for i, c in enumerate(celsius_q):
-#     print("{} °C = {} °F".format(c, fahrenheit_a[i]))
+for i, c in enumerate(celsius):
+    print(f"{c} °C = {fahrenheit[i]} °F")
 
+# the most basic NN - 1 layer with 1 neuron
+# later experiment with more layers and more neurons
 l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
 model = tf.keras.Sequential([l0])
 
@@ -21,16 +28,58 @@ model = tf.keras.Sequential([l0])
 #     tf.keras.layers.Dense(units=1, input_shape=[2])
 # ])
 
-model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+# "loss" is the loss function
+# optimizer will be explained some other time
+# later experiment with MeanSquaredError loss - it usually helps to achieve better results for this problem
+model.compile(
+    loss=tf.keras.losses.MeanAbsoluteError(),
+    optimizer=tf.keras.optimizers.Adam(0.1)
+)
 
-history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
-print("Finished training the model")
+# you can also use string values which will be converted to the corresponding objects with default values
+# model.compile(
+#     loss="mean_absolute_error",
+#     optimizer="adam"
+# )
 
-plt.xlabel('Epoch Number')
+model.summary()
+
+history = model.fit(celsius, fahrenheit, epochs=500, verbose=False)
+print("Training finished")
+
+plt.xlabel("Epoch Number")
 plt.ylabel("Loss Magnitude")
-plt.plot(history.history['loss'])
+plt.plot(history.history["loss"])
 plt.show()
 
-print(model.predict(np.array([22])))  # 71.6
+value = 22
+print("Predicted value:", model.predict([value]))
+print("Correct value:", value * 1.8 + 32)  # 71.6
 
-print("These are the l0 variables: {}".format(l0.get_weights()))
+print("These are the l0 variables:", l0.get_weights())
+# print("These are the l0 variables:", model.layers[0].get_weights())
+
+celsius_test = np.array([-45, -20, 5, 25, 40, 60])  # only values that were not in the train set
+fahrenheit_true = np.array([t * 1.8 + 32 for t in celsius_test])
+fahrenheit_predicted = np.array([])
+error = []
+# print(fahrenheit_true)
+
+for i, c in enumerate(celsius_test):
+    predicted = model.predict([np.array([c], dtype=float)])
+    error.append(abs(predicted - fahrenheit_true[i]))
+    fahrenheit_predicted = np.append(fahrenheit_predicted, predicted)
+
+print("Correct values:", fahrenheit_true)
+print("Predicted values:", fahrenheit_predicted)
+print()
+print("Absolute error values:", error)
+print("MAE:", np.average(error))
+
+mae = tf.keras.losses.MeanAbsoluteError()
+mae_val = mae(fahrenheit_true, fahrenheit_predicted).numpy()
+print("MAE:", mae_val)
+
+mse = tf.keras.losses.MeanSquaredError()
+mse_val = mse(fahrenheit_true, fahrenheit_predicted).numpy()
+print("MSE:", mse_val)
