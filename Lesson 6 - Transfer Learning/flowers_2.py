a0d7ab2f691f8f8a55802d46c0de2371fc485f7f
Milan Košťák <milankostak@seznam.cz>
2021-03-31 09:55:20 +0200

    flowers_2.py formatted

--- Lesson 6 - Transfer Learning/flowers_2.py
+++ Lesson 6 - Transfer Learning/flowers_2.py
@@ -3,14 +3,14 @@
 # solution
 # https://colab.research.google.com/github/tensorflow/examples/blob/master/courses/udacity_intro_to_tensorflow_for_deep_learning/l06c03_exercise_flowers_with_transfer_learning_solution.ipynb
 
-import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_datasets as tfds
-from tensorflow.keras import layers
-
-import numpy as np
-import matplotlib.pyplot as plt
 import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import tensorflow_hub as hub
+from tensorflow.keras import layers
 
 logger = tf.get_logger()
 logger.setLevel(logging.ERROR)
@@ -21,13 +21,13 @@ logger.setLevel(logging.ERROR)
 
 splits = tfds.Split.TRAIN.subsplit([70, 30])
 
-(training_set, validation_set), dataset_info = tfds.load('tf_flowers', with_info=True, as_supervised=True, split=splits)
+(training_set, validation_set), dataset_info = tfds.load("tf_flowers", with_info=True, as_supervised=True, split=splits)
 
 #
 # Print Information about the Flowers Dataset
 #
 
-num_classes = dataset_info.features['label'].num_classes
+num_classes = dataset_info.features["label"].num_classes
 
 num_training_examples = 0
 num_validation_examples = 0
@@ -38,12 +38,12 @@ for example in training_set:
 for example in validation_set:
     num_validation_examples += 1
 
-print('Total Number of Classes: {}'.format(num_classes))
-print('Total Number of Training Images: {}'.format(num_training_examples))
-print('Total Number of Validation Images: {} \n'.format(num_validation_examples))
+print(f"Total Number of Classes: {num_classes}")
+print(f"Total Number of Training Images: {num_training_examples}")
+print(f"Total Number of Validation Images: {num_validation_examples}")
 
 for i, example in enumerate(training_set.take(5)):
-    print('Image {} shape: {} label: {}'.format(i + 1, example[0].shape, example[1]))
+    print(f"Image {i + 1} shape: {example[0].shape} label: {example[1]}")
 
 #
 # Reformat Images and Create Batches
@@ -74,15 +74,15 @@ feature_extractor.trainable = False
 
 model = tf.keras.Sequential([
     feature_extractor,
-    layers.Dense(num_classes, activation='softmax')
+    layers.Dense(num_classes, activation="softmax")
 ])
 
 model.summary()
 
 model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy']
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
 )
 
 EPOCHS = 6
@@ -93,33 +93,33 @@ history = model.fit(train_batches, epochs=EPOCHS, validation_data=validation_bat
 # Plot Training and Validation Graphs
 #
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+acc = history.history["accuracy"]
+val_acc = history.history["val_accuracy"]
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
 
 epochs_range = range(EPOCHS)
 
 plt.figure(figsize=(8, 8))
 plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+plt.plot(epochs_range, acc, label="Training Accuracy")
+plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+plt.legend(loc="lower right")
+plt.title("Training and Validation Accuracy")
 
 plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
+plt.plot(epochs_range, loss, label="Training Loss")
+plt.plot(epochs_range, val_loss, label="Validation Loss")
+plt.legend(loc="upper right")
+plt.title("Training and Validation Loss")
 plt.show()
 
 #
 # Check Predictions
 #
 
-class_names = np.array(dataset_info.features['label'].names)
+class_names = np.array(dataset_info.features["label"].names)
 print("Class names: ", class_names)
 
 image_batch, label_batch = next(iter(train_batches))
@@ -132,8 +132,8 @@ predicted_ids = np.argmax(predicted_batch, axis=-1)
 predicted_class_names = class_names[predicted_ids]
 print("Predicted class names: ", predicted_class_names)
 
-print("Labels:           ", label_batch)
-print("Predicted labels: ", predicted_ids)
+print("Labels:          ", label_batch)
+print("Predicted labels:", predicted_ids)
 
 plt.figure(figsize=(10, 9))
 for n in range(30):
@@ -142,7 +142,7 @@ for n in range(30):
     plt.imshow(image_batch[n])
     color = "blue" if predicted_ids[n] == label_batch[n] else "red"
     plt.title(predicted_class_names[n].title(), color=color)
-    plt.axis('off')
+    plt.axis("off")
 _ = plt.suptitle("Model predictions (blue: correct, red: incorrect)")
 plt.show()
 
@@ -152,7 +152,7 @@ plt.show()
 
 IMAGE_RES = 299
 
-(training_set, validation_set), dataset_info = tfds.load('tf_flowers', with_info=True, as_supervised=True, split=splits)
+(training_set, validation_set), dataset_info = tfds.load("tf_flowers", with_info=True, as_supervised=True, split=splits)
 train_batches = training_set.shuffle(num_training_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
 validation_batches = validation_set.map(format_image).batch(BATCH_SIZE).prefetch(1)
 
@@ -161,15 +161,15 @@ feature_extractor = hub.KerasLayer(URL, input_shape=(IMAGE_RES, IMAGE_RES, 3), t
 
 model_inception = tf.keras.Sequential([
     feature_extractor,
-    tf.keras.layers.Dense(num_classes, activation='softmax')
+    tf.keras.layers.Dense(num_classes, activation="softmax")
 ])
 
 model_inception.summary()
 
 model_inception.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy']
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
 )
 
 EPOCHS = 6
